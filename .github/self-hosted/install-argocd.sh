29514fe60936996da296bb417c438d8bebb97073
Admir Trakic <atrakic@users.noreply.github.com>
2022-10-28 22:22:12 +0200

    Update

--- .github/self-hosted/install-argocd.sh
+++ .github/self-hosted/install-argocd.sh
@@ -5,12 +5,38 @@ set -o pipefail
 
 NS=argocd
 
-kubectl create namespace "$NS"
+kubectl create namespace "$NS" || true
 kubectl apply -n "$NS" -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl wait --for=condition=available deployment -l "app.kubernetes.io/name=argocd-server" -n "$NS" --timeout=600s
 kubectl wait --for=condition=available deployment argocd-repo-server -n "$NS" --timeout=60s
 kubectl wait --for=condition=available deployment argocd-dex-server -n "$NS" --timeout=60s
 
+# disable TLS on argocd server
 kubectl -n "$NS" patch deployment argocd-server --type json \
   -p='[ { "op": "replace", "path":"/spec/template/spec/containers/0/command","value": ["argocd-server","--staticassets","/shared/app","--insecure"] }]'
+
+# enable ingress:
+cat <<EOF | kubectl -n "$NS" apply -f -
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: argocd-server-http-ingress
+  annotations:
+    nginx.ingress.kubernetes.io/rewrite-target: /$1
+    nginx.ingress.kubernetes.io/backend-protocol: "HTTP"
+spec:
+  ingressClassName: nginx
+  rules:
+    - host: argocd.mydomain.com
+      http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: argocd-server
+                port:
+                  number: 80
+EOF
--- .github/self-hosted/setup.sh
+++ .github/self-hosted/setup.sh
@@ -4,13 +4,47 @@ set -e
 set -o pipefail
 
 minikube delete || true
-	minikube start \
-		--driver=docker \
-		--kubernetes-version=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt) \
-		--memory=8192 \
-		--bootstrapper=kubeadm \
-		 --extra-config=kubeadm.node-name=minikube \
-		 --extra-config=kubelet.hostname-override=minikube
+
+docker network rm minikube || true
+#docker network create --driver=bridge --subnet=172.28.0.0/16 --gateway=172.28.0.1 minikube
+
+minikube start \
+  --driver=docker \
+  --kubernetes-version="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)" \
+  --memory=8192 \
+  --bootstrapper=kubeadm \
+  --extra-config=kubeadm.node-name=minikube \
+  --extra-config=kubelet.hostname-override=minikube
+  #--network minikube \
 
 ## Addons:
 minikube addons disable metrics-server
+
+
+example::ingress() {
+  minikube addons enable ingress
+  minikube addons enable ingress-dns
+  #minikube tunnel &
+  kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0
+  kubectl expose deployment web --type=NodePort --port=8080
+  cat <<EOF | kubectl apply -f -
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: example-ingress
+  annotations:
+    nginx.ingress.kubernetes.io/rewrite-target: /$1
+spec:
+  rules:
+    - host: hello-world.info
+      http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: web
+                port:
+                  number: 8080
+EOF
+}
